a4f3750e58bd6f29cc8a0bbc7c358830ee606d6c
dzmchar <chernyavsky_work@mail.ru>
2019-01-23 14:13:01 +0300

    improve palindromes

--- src/palindromes/index.ts
+++ src/palindromes/index.ts
@@ -1,8 +1,5 @@
 export const isPalindrome = (string: string): boolean => {
-  const pristineString = string
-    .toLowerCase()
-    .match(/\w/g)
-    .join("");
+  const pristineString = string.toLowerCase().replace(/[^\w]/g, "");
   const invertedPristineString = pristineString
     .split("")
     .reverse()
