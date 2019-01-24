54cb2b82d3af241e1339fbc293c9854e6fdc5d9b
dzmchar <chernyavsky_work@mail.ru>
2019-01-24 17:51:04 +0300

    rewrite palindrome suite using FunctionalTestSuite

--- tests/palindromes.spec.ts
+++ tests/palindromes.spec.ts
@@ -1,20 +1,24 @@
-import { expect } from "chai";
-import "mocha";
-
+import { FunctionalTestSuite } from ".";
 import { isPalindrome } from "../src/palindromes";
 
 const palindrome = "papap";
 const complexPalindrome = "Dogma? I am God";
 const invalidString = "An invalid string";
 
-describe("Palindromes", () => {
-  it("should correctly detect one-word palindromes", () => {
-    expect(isPalindrome(palindrome)).to.eq(true);
-  });
-  it("should correctly detect phrase palindromes", () => {
-    expect(isPalindrome(complexPalindrome)).to.eq(true);
-  });
-  it("should correctly detect non-palindromes", () => {
-    expect(isPalindrome(invalidString)).to.eq(false);
-  });
-});
+const palindromeSuite = new FunctionalTestSuite("Palindromes", isPalindrome)
+  .addCase({
+    expectedResult: true,
+    testCaseLabel: "Simple one word palindrome",
+    testInput: palindrome
+  })
+  .addCase({
+    expectedResult: true,
+    testCaseLabel: "Phrase palindrome",
+    testInput: complexPalindrome
+  })
+  .addCase({
+    expectedResult: false,
+    testCaseLabel: "Invalid string should not pass",
+    testInput: invalidString
+  })
+  .run();
