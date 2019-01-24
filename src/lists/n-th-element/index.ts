c3a7a4aa362adfde89e861d330c7f70e9fe1065b
dzmchar <chernyavsky_work@mail.ru>
2019-01-24 16:57:42 +0300

    n-th list element

---
+++ src/lists/n-th-element/index.ts
@@ -0,0 +1,6 @@
+interface INthParameters {
+  list: any[];
+  th: number;
+}
+export const nth = (params: INthParameters): any[] =>
+  params.list.filter((item, index) => (index+1) % params.th === 0);
---
+++ src/lists/n-th-element/n-th-element.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import "mocha";
+
+import { nth } from "./";
+
+const list = [1, 2, 3, 4, 5, 6, 7, 8];
+describe("nth", () => {
+  it("should select 3rd element in the list", () => {
+    expect(nth({ list, th: 3 })).to.eql([3, 6]);
+  });
+  it("should select 2nd element in the list", () => {
+    expect(nth({ list, th: 2 })).to.eql([2, 4, 6, 8]);
+  });
+});
