93e5b147e227fee4a77c7bfe844289a0ceaf04e8
dzmchar <chernyavsky_work@mail.ru>
2019-01-24 17:59:40 +0300

    rewrite n-th list element spec to use FST

--- tests/n-th-element.spec.ts
+++ tests/n-th-element.spec.ts
@@ -1,14 +1,16 @@
-import { expect } from "chai";
-import "mocha";
-
+import { FunctionalTestSuite } from ".";
 import { nth } from "../src/lists/n-th-element";
 
 const list = [1, 2, 3, 4, 5, 6, 7, 8];
-describe("nth", () => {
-  it("should select 3rd element in the list", () => {
-    expect(nth({ list, th: 3 })).to.eql([3, 6]);
-  });
-  it("should select 2nd element in the list", () => {
-    expect(nth({ list, th: 2 })).to.eql([2, 4, 6, 8]);
-  });
-});
+const nthElementSuite = new FunctionalTestSuite("n-th list element", nth)
+  .addCase({
+    expectedResult: [3, 6],
+    testCaseLabel: "should select 3rd element in the list",
+    testInput: { list, th: 3 }
+  })
+  .addCase({
+    expectedResult: [2, 4, 6, 8],
+    testCaseLabel: "should select 2nd element in the list",
+    testInput: { list, th: 2 }
+  })
+  .run();
