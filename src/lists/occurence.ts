114a1e83ba1708d401bda98dec0afb78787a59a9
dzmchar <chernyavsky_work@mail.ru>
2019-01-25 12:38:05 +0300

    list item occurrence counter

---
+++ src/lists/occurence.ts
@@ -0,0 +1,22 @@
+interface IOccurenceCounterProps {
+  list: any[];
+  value: any;
+}
+
+/**
+ * Count occurences of a value in a list
+ *
+ * @export
+ * @param {IOccurenceCounterProps} {list,value}
+ * @returns {number}
+ * @see https://jsperf.com/simple-unique-detection
+ */
+export function occurence({ list, value }: IOccurenceCounterProps): number {
+  let counter = 0;
+  for (const i of list) {
+    if (i === value) {
+      counter += 1;
+    }
+  }
+  return counter;
+}
---
+++ tests/lists-occurence.spec.ts
@@ -0,0 +1,10 @@
+import { FunctionalTestSuite } from ".";
+import { occurence } from "../src/lists/occurence";
+
+const occurenceTestSuite = new FunctionalTestSuite("lists-occurence", occurence)
+  .addCase({
+    expectedResult: 4,
+    testCaseLabel: "Simple occurence counter",
+    testInput: { list: [2, 2, 2, 2, 3], value: 2 }
+  })
+  .run();
