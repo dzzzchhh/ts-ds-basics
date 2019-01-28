d97cb6489a42b5493bb2327da15c1d01c53802ab
dzmchar <chernyavsky_work@mail.ru>
2019-01-28 10:00:03 +0300

    example doc support for lists-n-th element

--- src/lists/n-th-element.ts
+++ src/lists/n-th-element.ts
@@ -9,6 +9,10 @@ interface INthParameters {
  * @param {Array} list  List to process
  * @param {number} th Every "th" shall be selected
  * @returns {Array} List with every n-th element
+ *
+ * @example
+ *
+ * nth({list:[1,2,3,4,5,6,7,8],th:4}) -> [4,8]
  */
 export const nth = ({ list, th }: INthParameters): any[] =>
   list.filter((item, index) => (index + 1) % th === 0);
