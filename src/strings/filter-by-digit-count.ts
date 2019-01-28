3537f608a6caf2d0bfe6ecfdeaaecc8be12ca2aa
dzmchar <chernyavsky_work@mail.ru>
2019-01-28 16:20:03 +0300

    docstring for digit count filter

--- src/strings/filter-by-digit-count.ts
+++ src/strings/filter-by-digit-count.ts
@@ -3,12 +3,21 @@ interface IDigitFilterProps {
   digits: number;
 }
 
+/**
+ * Given a string and number of digits removes all numbers that have more digits than required
+ *
+ * @param {string} characterSequence
+ * @param {number} digits
+ * @export
+ * @example
+ *
+ * filterByDigitCount({characterSequence:"100 200 2000 3000",digits:4}) // -> "100 200"
+ */
 export function filterByDigitCount({
   characterSequence,
   digits
 }: IDigitFilterProps) {
   // TODO: optimize regexp
   const replaceMatcher = new RegExp(`\\d{${digits},} | \\d{${digits},}`, "g");
-  const res = characterSequence.replace(replaceMatcher, "");
-  return res;
+  return characterSequence.replace(replaceMatcher, "");
 }
