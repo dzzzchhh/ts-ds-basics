3752cdb48a7b0eed2f6d5b570e802267e81f6dbe
dzmchar <chernyavsky_work@mail.ru>
2019-01-30 16:41:59 +0300

    improve docs for FTS

--- tests/index.ts
+++ tests/index.ts
@@ -1,25 +1,50 @@
 import { expect } from "chai";
 import "mocha";
 
-interface ITestCase {
+/**
+ * Defines general shape of a functional test case
+ * @interface IFunctionalTestCase
+ */
+interface IFunctionalTestCase {
   testCaseLabel?: string;
   testInput: any;
   expectedResult: any;
 }
 
+/**
+ * Compose dynamic test suites for single-arity functions
+ * @export
+ * @class FunctionalTestSuite
+ */
 export class FunctionalTestSuite {
-  private testCases: ITestCase[] = [];
+  /**
+   * List of test cases for a suite
+   * @private
+   * @type {IFunctionalTestCase[]}
+   * @memberof FunctionalTestSuite
+   */
+  private testCases: IFunctionalTestCase[] = [];
   constructor(
     private suiteName: string,
     private testFunction: (input: any) => any
   ) {}
-  public addCase(testCase: ITestCase) {
+  /**
+   * Adds a new test case to a test suite
+   * @param {IFunctionalTestCase} testCase
+   * @returns
+   * @memberof FunctionalTestSuite
+   */
+  public addCase(testCase: IFunctionalTestCase) {
     this.testCases.push({
       ...testCase,
       testCaseLabel: testCase.testCaseLabel || testCase.testInput
     });
     return this;
   }
+  /**
+   * Register and initiate test suite making it visible for test executor
+   * @memberof FunctionalTestSuite
+   */
   public run() {
     describe(this.suiteName, () => {
       this.testCases.forEach(testCase => {
