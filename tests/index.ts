f9397c0ed55cb304c6513fb6d89206e1e7c0ee7c
dzmchar <chernyavsky_work@mail.ru>
2019-01-31 14:10:46 +0300

    extend FTS with ability to pass lists of arguments to be appled to suite function

--- tests/index.ts
+++ tests/index.ts
@@ -7,6 +7,20 @@ import "mocha";
  */
 interface IFunctionalTestCase {
   testCaseLabel?: string;
+  /**
+   * Value to be used as this argument for case when test function should be called with .apply
+   * @type {*}
+   * @memberof IFunctionalTestCase
+   */
+  testCaseContext?: any;
+  /**
+   * Describes whether or not testInput list should be applied to test suite function
+   * @type {boolean}
+   * @memberof IFunctionalTestCase
+   * @example
+   * testFunction.apply(testCaseContext,testInput)
+   */
+  applyArgumentsList?: boolean;
   testInput: any;
   expectedResult: any;
 }
@@ -26,7 +40,7 @@ export class FunctionalTestSuite {
   private testCases: IFunctionalTestCase[] = [];
   constructor(
     private suiteName: string,
-    private testFunction: (input: any) => any
+    private testFunction: (...input: any) => any
   ) {}
   /**
    * Adds a new test case to a test suite
@@ -49,6 +63,16 @@ export class FunctionalTestSuite {
     describe(this.suiteName, () => {
       this.testCases.forEach(testCase => {
         it(testCase.testCaseLabel, () => {
+          if (testCase.applyArgumentsList) {
+            const defaultContext = {};
+            expect(
+              this.testFunction.apply(
+                testCase.testCaseContext || defaultContext,
+                testCase.testInput
+              )
+            ).to.eql(testCase.expectedResult);
+            return;
+          }
           expect(this.testFunction(testCase.testInput)).to.eql(
             testCase.expectedResult
           );
