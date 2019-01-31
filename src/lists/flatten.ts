e4172d1880b2c3cca46afe2f05b029a8d0a41d31
dzmchar <chernyavsky_work@mail.ru>
2019-01-31 10:29:10 +0300

    improve list flatten example

--- src/lists/flatten.ts
+++ src/lists/flatten.ts
@@ -9,5 +9,5 @@
  * flatten([[1,2],[3,4]]) -> [1,2,3,4]
  */
 export function flatten(list: any[]): any[] {
-  return list.reduce((acc, curr) => [...acc, ...curr], []);
+  return Array.prototype.concat.apply([], list);
 }
