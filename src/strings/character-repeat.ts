88969a4e6763d677674ca8a7202d301a6d7c8361
dzmchar <chernyavsky_work@mail.ru>
2019-02-04 10:29:28 +0300

    improve docs for compose character sequence

--- src/strings/character-repeat.ts
+++ src/strings/character-repeat.ts
@@ -3,6 +3,17 @@ export interface ICharacterSequenceComposer {
   repeatCount: number;
 }
 
+/**
+ * Given a string and a (n) number of repetitions
+ * composes a string where original string
+ * is repeated (n) times
+ * @export
+ * @param {ICharacterSequenceComposer} {
+ *   sequence,
+ *   repeatCount
+ * }
+ * @returns {string}
+ */
 export function composeCharacterSequence({
   sequence,
   repeatCount
