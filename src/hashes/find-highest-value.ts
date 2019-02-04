1ebed5ae070989bec84d87469c1539cf01d23f63
dzmchar <chernyavsky_work@mail.ru>
2019-02-04 15:47:31 +0300

    improve docs for highest hash value

--- src/hashes/find-highest-value.ts
+++ src/hashes/find-highest-value.ts
@@ -1,6 +1,10 @@
 /**
- * Given a <string,number> map of values, return the key name of the pair with the highest property value
+ * Given a <string,number> map of values
+ * return the name of the key for the pair with the highest property value
  * @param {object} hash
+ * @export
+ * @example
+ * findHighestHashValue({Jack:25,Suzan:30}) // -> "Suzan"
  */
 export function findHighestHashValue(hash: object) {
   const highestValue = Math.max.apply(Math, Object.values(hash));
