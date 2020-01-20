1212d040d8d4fcb5f5f269e5c121530241420dab
dzmitry_charniauski <dzmitry_charniauski1@epam.com>
2020-01-20 15:28:20 +0300

    enable duplicates sorting for Quicksort

--- src/sorting/quicksort.ts
+++ src/sorting/quicksort.ts
@@ -1,17 +1,26 @@
 /**
  * Simplest straight-forward implementation of quicksort
- * @todo Handle duplicate list elements
- * @param list
+ * Able to handle duplicate entries
+ * @param {Array<number>} list Source list
+ * @returns {Array<number>} sorted list
  */
-export function quickSort(list: number[]) {
+export function quickSort(list: number[]): number[] {
   if (list.length === 0) {
-    return [];
+    return []; // stop the recursion for emtpy lists
   }
+
+  // 1. Find root index (RI)
   const median = Math.floor(list.length / 2);
+  // 2. Find root index element (RIE)
   const centerElement = list[median];
 
+  // 3. Find all items that are greater and less then RIE
   const less = list.filter(i => i < centerElement);
   const more = list.filter(i => i > centerElement);
+  // 4. Find all items that are duplicates of the RIE
+  const equal = list.filter(i => i === centerElement);
 
-  return [...quickSort(less), centerElement, ...quickSort(more)];
+  // 5. Sort all the items that are less or greater
+  // as well as spread the duplicates of RIE
+  return [...quickSort(less), ...equal, ...quickSort(more)];
 }
--- tests/sorting-quicksort.spec.ts
+++ tests/sorting-quicksort.spec.ts
@@ -17,4 +17,14 @@ new FunctionalTestSuite<number[], number[]>("Quicksort", quickSort)
     testInput: [[5, 100, 2, 10, 1]],
     expectedResult: [1, 2, 5, 10, 100]
   })
+  .addCase({
+    testCaseLabel: "With duplicates",
+    testInput: [[5, 100, 2, 1, 10, 5, 1]],
+    expectedResult: [1, 1, 2, 5, 5, 10, 100]
+  })
+  .addCase({
+    testCaseLabel: "Duplicates only",
+    testInput: [[1, 1, 1, 1, 1]],
+    expectedResult: [1, 1, 1, 1, 1]
+  })
   .run();
